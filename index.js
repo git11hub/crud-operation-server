6d6713a84807e4a57caf9c5ffc8ea3a3d969f8d8
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-07-03 14:00:26 +0600

    delete operation successful

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 const uri = "mongodb+srv://crudProject:crud1234@cluster0.eftco.mongodb.net/crudProject?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,20 +20,28 @@ app.listen(5000);
 client.connect(err => {
   const userCollection = client.db("crudProject").collection("crudTesting");
     
-  app.post("/addUser", (req, res) => {
-      
+  app.post("/addUser", (req, res) => {      
     const user = req.body;
     userCollection.insertOne(user)
     .then(result => {
       console.log(result);
     })
   });
+ 
   
   app.get('/users', (req, res) => {
     userCollection.find()
       .toArray((err, items) => {
         res.send(items);
-        console.log('from database hoho', items);
+        // console.log('from database hoho', items);
       })
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    userCollection.deleteOne({_id:ObjectId(req.params.id)})
+    .then( result => {
+      console.log(result);
+    })
+    // console.log(req.params.id);
   })
 });
